1e135ec1c940f19003775e9be03ca31cc2f4b0fa
Ga <gabri.magni.97@gmail.com>
2022-03-16 14:15:52 +0100

    snak 3

---
+++ snak 3/asset/js/main.js
@@ -0,0 +1,16 @@
+let container = document.getElementById("container");
+let sum = 0;
+let numbers = [1, 2, 3, 4, 9];
+
+//metodo alternativo, in questo caso ricorda di lasciare vuoto l'array
+/*for (i = 0; i < 10; i++) {
+    let randomNum = Math.floor(Math.random() * 100) + 1;
+    numbers.push(randomNum);
+    sum += numbers[i];
+}
+container.innerHTML = `${sum}`*/
+
+for (i = 0; i < numbers.lenght; i = i + 2) {
+    sum += numbers[i]
+    container.innerHTML = `${sum}`
+}
